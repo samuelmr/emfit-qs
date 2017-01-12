d2e6f7be1dd04e12a53c05a72d5f387c170fe78a
Samuel Rinnetmäki <samuel@iki.fi>
2017-01-12 22:56:50 +0200

    added login, status and statuses

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const request = require('request-promise'),
   config = require('./config')
 
+// request.debug = true
+
 function Client(token) {
   this.token = token
   this.request = request.defaults({
@@ -12,9 +14,45 @@ function Client(token) {
   })
 }
 
+Client.prototype.login = function (username, password) {
+  var self = this
+  var opts = {
+    baseUrl: config.baseUrl,
+    uri: 'api/v1/login',
+    form: {
+      username: username,
+      password: password
+    },
+    transform: function (response) {
+      return JSON.parse(response)
+    }
+  }
+  return request.post(opts).then(function(data) {
+    self.token = data.remember_token
+    return data
+  })
+}
+
+
+
+
 Client.prototype.user = function () {
   var opts = {
     uri: '/api/v1/user/get',
+  }
+  return this.request.get(opts)
+}
+
+Client.prototype.status = function (deviceId) {
+  var opts = {
+    uri: '/api/v1/device/status/' + deviceId
+  }
+  return this.request.get(opts)
+}
+
+Client.prototype.statuses = function () {
+  var opts = {
+    uri: '/v2/device/status',
   }
   return this.request.get(opts)
 }
